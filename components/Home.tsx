72fcd1054815735c19591be671ecd76649a82bf8
thegitparticle <maddamsettysanjit@gmail.com>
2022-11-08 19:58:44 +0530

    add: styles to home page setup + enhance: custom values input for nft details

--- components/Home.tsx
+++ components/Home.tsx
@@ -61,16 +61,6 @@ function createTokenPayload(
 }
 
 export default function Home() {
-	const [nftName, setNftName] = useState("silnft1");
-	const [nftDescription, setNftDescription] = useState(
-		"quick-nft-test-creations"
-	);
-	const [nftUri, setNftUri] = useState(
-		"https://images.unsplash.com/photo-1667249744516-d604786b8e4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-	);
-
-	const supply = 1;
-
 	const {
 		connect,
 		disconnect,
@@ -83,45 +73,132 @@ export default function Home() {
 	} = useWallet();
 	const client = new AptosClient(NODE_URL);
 
-	// tokenClient is used to interact with the token contract
-	const tokenClient = new TokenClient(client);
-	// coinClient is used to interact with the coin contract for checking balances
-	const coinClient = new CoinClient(client);
+	function TabBar() {
+		return (
+			<div className="flex sticky top-0 z-40 w-full flex-row duration-500 items-center justify-between">
+				<p className="text-2xl text-emerald-800 font-body font-bold mx-24 my-6">
+					QuickNFT on Aptos
+				</p>
+				<p className="text-md text-emerald-500 font-body font-bold mx-24 my-6">
+					{account.address.slice(0, 5) +
+						"..." +
+						account.address.slice(
+							account.address.length - 5,
+							account.address.length
+						)}
+				</p>
+			</div>
+		);
+	}
 
-	const createNewNft = async () => {
-		// Create a new collection
-		signAndSubmitTransaction(
-			createCollectionPayload(
-				nftName + " collection",
-				nftDescription,
-				nftUri
-			),
-			{ gas_unit_price: 100 }
-		)
-			.then((res) => {
-				// create a new NFT
-				signAndSubmitTransaction(
-					createTokenPayload(
-						nftName + " collection",
-						nftName,
-						nftDescription,
-						nftUri,
-						""
-					),
-					{ gas_unit_price: 100 }
-				).catch((err) => console.log("nft minting failed" + err));
-			})
-			.catch((e) => console.log("collection creation failed" + e));
-	};
+	function MintNFT() {
+		const [nftName, setNftName] = useState("");
+		const [nftDescription, setNftDescription] = useState("");
+		const [nftUri, setNftUri] = useState("");
+
+		const updateName = (event) => {
+			setNftName(event.target.value);
+		};
+
+		const updateUri = (event) => {
+			setNftUri(event.target.value);
+		};
+
+		const updateDescription = (event) => {
+			setNftDescription(event.target.value);
+		};
+
+		const supply = 1;
+
+		const createNewNft = async () => {
+			// Create a new collection
+			signAndSubmitTransaction(
+				createCollectionPayload(
+					nftName + " collection",
+					nftDescription,
+					nftUri
+				),
+				{ gas_unit_price: 100 }
+			)
+				.then((res) => {
+					// create a new NFT
+					signAndSubmitTransaction(
+						createTokenPayload(
+							nftName + " collection",
+							nftName,
+							nftDescription,
+							nftUri,
+							""
+						),
+						{ gas_unit_price: 100 }
+					).catch((err) => console.log("nft minting failed" + err));
+				})
+				.catch((e) => console.log("collection creation failed" + e));
+		};
+
+		return (
+			<div className="flex flex-col items-center justify-center w-full h-screen">
+				<p className="text-md text-emerald-500 font-body font-bold my-4">
+					Make an NFT of any media asset with a link instantly!
+				</p>
+				<div className="flex items-start flex-col w-3/6 my-4">
+					<p className="text-md text-emerald-500 font-body font-bold my-1">
+						Paste the url of media asset
+					</p>
+					<input
+						type="uri"
+						id="uri"
+						name="uri"
+						required
+						minLength={3}
+						maxLength={100}
+						onChange={(event) => updateUri(event)}
+						className="flex h-10 w-full bg-emerald-800/25 text-md text-emerald-500 font-body font-bold text-center rounded-xl"
+					/>
+				</div>
+				<div className="flex items-start flex-col w-3/6 my-4">
+					<p className="text-md text-emerald-500 font-body font-bold my-1">
+						Name your NFT
+					</p>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						required
+						minLength={3}
+						maxLength={30}
+						onChange={updateName}
+						className="flex h-10 w-full bg-emerald-800/25 text-md text-emerald-500 font-body font-bold text-center rounded-xl"
+					/>
+				</div>
+				<div className="flex items-start flex-col w-3/6 my-4">
+					<p className="text-md text-emerald-500 font-body font-bold my-1">
+						Description (optional)
+					</p>
+					<input
+						type="text"
+						id="description"
+						name="description"
+						minLength={3}
+						maxLength={100}
+						onChange={updateDescription}
+						className="flex h-10 w-full bg-emerald-800/25 text-md text-emerald-500 font-body font-bold text-center rounded-xl"
+					/>
+				</div>
+				<button
+					onClick={() => createNewNft()}
+					className="flex py-3 px-6 bg-emerald-700 rounded-full text-emerald-100 font-body font-bold text-md my-4"
+				>
+					Mint NFT
+				</button>
+			</div>
+		);
+	}
 
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<p className="text-lg text-emerald-800">QuickNFT</p>
-			<p className="text-sm text-emerald-500">
-				Make an NFT of any media asset with a link instantly!
-			</p>
-			<p className="text-sm text-emerald-500">{account.address}</p>
-			<button onClick={() => createNewNft()}>Mint NFT</button>
+		<div className="flex flex-col items-center justify-center w-screen h-full absolute">
+			<TabBar />
+			<MintNFT />
 		</div>
 	);
 }
